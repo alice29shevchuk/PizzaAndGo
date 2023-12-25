8c6fbdcbcd5abc95e5a7f304b521df9eefb1b937
alice29shevchuk <Sevc_sd76@student.itstep.org>
2023-12-25 17:25:36 +0200

    added component skeleton for loading

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Sort from './components/Sort';
 import PizzaCard from './components/PizzaCard';
 // import pizzas from './assets/pizzas.json';
 import React from 'react';
+import Skeleton from './components/Skeleton';
 function App() {
   const [pizzas,setPizzas] = React.useState([]);
+  const [isLoading,setIsLoading] = React.useState(true);
   React.useEffect(()=>{
     fetch('https://6589685a324d41715258e658.mockapi.io/pizzas')
     .then((response)=>{
@@ -16,6 +18,7 @@ function App() {
     })
     .then((data)=>{
       setPizzas(data);
+      setIsLoading(false);
     })
   },[]);
   return (
@@ -30,10 +33,9 @@ function App() {
           <h2 className="content__title">Меню</h2>
           <div className="content__items">
           {
-            pizzas.map((obj)=>(
-            // <PizzaCard title = {obj.title} price={obj.price} imgUrl={obj.imageUrl} sizes = {obj.sizes} types={obj.types}/>
-            <PizzaCard key={obj.id} {...obj}/>
-            ))
+            isLoading 
+            ? [...new Array(6)].map((_,index)=><Skeleton key={index}/>)
+            :pizzas.map((obj)=><PizzaCard key={obj.id} {...obj}/>)
           }
           </div>
         </div>
@@ -43,3 +45,4 @@ function App() {
 }
 
 export default App;
+// <PizzaCard key={obj.id} title = {obj.title} price={obj.price} imgUrl={obj.imageUrl} sizes = {obj.sizes} types={obj.types}/>
